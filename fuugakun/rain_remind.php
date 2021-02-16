e547fd8b97254ca8785d705b7f7f1f05506513b3
takashimurata <fuuga09060906@gmail.com>
2021-02-16 19:28:50 +0900

    [add] 雨とリマインドした後、保存したdeparture_timeをNULLに更新する。
    cron処理はまだ入れていません。
    cron処理は5分ごとに流します。
    リマインド時間は、出発の10分~5分前に行うようにしています。
    関数名、コードのご確認をお願い致します。

--- fuugakun/rain_remind.php
+++ fuugakun/rain_remind.php
@@ -7,44 +7,51 @@ require './vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$search_time = date("Y-m-d K:i", strtotime("-10 minute"));
-$sql = 'SELECT `line_accesstoken` FROM `users` WHERE :search_time <= `departure_time`';
+$search_time = date("Y-m-d H:i", strtotime("+10 minute"));
+$sql = 'SELECT `line_accesstoken` FROM `users` WHERE :search_time >= `departure_time`';
 $stmt = $dbh->prepare($sql);
 $stmt->bindValue(':search_time', $search_time);
 $stmt->execute();
-$fetch_line_accesstoken = $stmt->fetchall(PDO::FETCH_ASSOC);
-$line_accesstoken = array_column($fetch_line_accesstoken, 'line_accesstoken');
-//TODO::リマインド後、departure_timeを削除。複数ユーザーを想定。SQLは最小限に。
-//TODO::cron処理を5分ごとに流す。
+$fetch_line_accesstokens = $stmt->fetchall(PDO::FETCH_ASSOC);
+$line_accesstokens = array_column($fetch_line_accesstokens, 'line_accesstoken');
 
-/*
-   var_dump($line_accesstoken);
- */
-
+//リプライ
 $reply_message = [
-  'to' => $line_accesstoken,
-  'messages' => [
-    [
-      'type' => 'text',
-      'text' => '傘忘れたらあかんでー！！！'
-    ]
-  ]
+	'to' => $line_accesstokens,
+	'messages' => [
+		[
+			'type' => 'text',
+			'text' => '傘忘れたらあかんでー！！！'
+		]
+	]
 ];
 
+//TODO::リファクタ予定
+//リマインドした場合、departure_timeをNULLへ更新
+if (!empty($line_accesstokens)) {
+	$update_null_conditional_branches = '';
+	foreach ($line_accesstokens as $line_accesstoken) {
+		$update_null_conditional_branches .= 'WHEN `line_accesstoken` = "' . $line_accesstoken .  '" THEN NULL ';
+	}
+	$update_string = 'UPDATE `users` SET `departure_time` = CASE ' . $update_null_conditional_branches;
+	$update_to_null_sql = $update_string . ' ELSE `departure_time` END';
+	$stmt = $dbh->query($update_to_null_sql);
+}
+
+//Curl処理
 $reply_message = json_encode($reply_message);
 $ch = curl_init('https://api.line.me/v2/bot/message/multicast');
 $options = [
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_BINARYTRANSFER => true,
-  CURLOPT_HEADER => true,
-  CURLOPT_POSTFIELDS => $reply_message,
+	CURLOPT_CUSTOMREQUEST => 'POST',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_BINARYTRANSFER => true,
+	CURLOPT_HEADER => true,
+	CURLOPT_POSTFIELDS => $reply_message,
 ];
 
 curl_setopt_array($ch, $options);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  'Content-Type: application/json; charser=UTF-8',
-  'Authorization: Bearer ' . $_ENV["ACCESSTOKEN"]
+	'Content-Type: application/json; charser=UTF-8',
+	'Authorization: Bearer ' . $_ENV["ACCESSTOKEN"]
 ));
 curl_exec($ch);
-
